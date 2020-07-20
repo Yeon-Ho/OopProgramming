f35aaa009f284cfeb7143765553d51747853ba5c
user2 <user2@user2-PC>
2020-07-20 21:49:52 +0900

    first

---
+++ OopProgramming3/src/capsule/Accounting.java
@@ -0,0 +1,64 @@
+package capsule;
+
+public class Accounting {
+
+	private int balance; // 잔고
+	private int salesPrce; // 매출
+	private int expences;//지출
+	
+	//기본 생성자
+	public Accounting() { }
+
+	//Getter , Setter
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	public int getSalesPrce() {
+		return salesPrce;
+	}
+
+	public void setSalesPrce(int salesPrce) {
+		this.salesPrce = salesPrce;
+	}
+
+	public int getExpences() {
+		return expences;
+	}
+
+	public void setExpences(int expences) {
+		this.expences = expences;
+	}
+	
+	// 매출등록을 담당하는 메소드
+	public int registerSales(int payPrice) {
+		
+		balance += payPrice;
+		salesPrce += payPrice;
+		
+		return balance;
+	}
+	
+	//지출등록을 담당하는 메소드
+	public boolean registerExpences(int expencesPrice) {
+		
+		if(balance > expencesPrice) {
+			balance -= expencesPrice;
+			expences += expencesPrice;
+			return true;
+		}else {
+			return false;
+		}
+		
+	}
+	
+
+	
+	
+	
+	
+}
